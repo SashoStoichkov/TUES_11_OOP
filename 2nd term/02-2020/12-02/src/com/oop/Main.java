a020c5b3df70578c54393b0a7d68d4329b2aea05
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-02-12 12:28:59 +0200

    date typo

--- 2nd term/02-2020/12-02/src/com/oop/Main.java
+++ 2nd term/02-2020/12-02/src/com/oop/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println("Testing" + 1 + 2 + 3);
+        System.out.println("Hello there!");
     }
 }
